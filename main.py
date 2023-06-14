f1519203c2887e9ceb83c9f30d3ff9e72e56c65a
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-13 22:34:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,18 @@
-import sys
-import threading
-import tkinter as tk
+import speech_recognition as sr
 
-import speech_recognition
-import pyttsx3 as tts
-from neuralintents import GenericAssistant
+r = sr.Recognizer
 
+while True:
+    try:
+        with sr.Microphone() as source:
+            print("Say something")
+            audio = r.listen(source)
+            text = r.recognize_google(audio)
+            text = text.lower()
 
-class Assistant:
+            print(f"Recognized text:{text}")
 
-    def __init__(self):
-        self.recognizer = speech_recognition.Recognizer()
-        self.speaker = tts.init()
-        self.speaker.setProperty("rate", 150)
-
-        self.assistant = GenericAssistant("intents.json", intent_methods={"file": self.create_file})
-        self.assistant.train_model()
-
-        self.root = tk.Tk()
-        photo = tk.PhotoImage(file="claptrap.png")
-        self.image = photo
-        self.label = tk.Label(image=photo)
-        self.label.pack()
-
-        threading.Thread(target=self.run_assistant).start()
-        self.root.mainloop()
-
-    def create_file(self):
-        pass
-
-    def run_assistant(self):
-        while True:
-            try:
-                with speech_recognition.Microphone() as mic:
-                    self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                    audio = self.recognizer.listen(mic)
-
-                    text = self.recognizer.recognize_google(audio)
-                    text = text.lower()
-
-                    if "claptrap" in text:
-                        self.label.config(fg="red")
-                        audio = self.recognizer.listen(mic)
-                        text = self.recognizer.recognize_google(audio)
-                        text = text.lower()
-                        if text == "stop":
-                            self.speaker.say("See ya")
-                            self.speaker.runAndWait()
-                            self.speaker.stop()
-                            self.root.destroy()
-                            sys.exit()
-                        else:
-                            if text is not None:
-                                response = self.assistant.request(text)
-                                if response is not None:
-                                    self.speaker.say(response)
-                                    self.speaker.runAndWait()
-                                self.label.config(fg="black")
-            except:
-                self.label.config(fg="black")
-                continue
-
-
-Assistant()
+    except:
+        print("No audio detected")
+        r = sr.Recognizer()
+        continue
