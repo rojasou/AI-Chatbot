d56af18cb85fd212db9625a092f017af597f41ef
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-14 23:37:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import speech_recognition as sr
+import pyttsx3
 
 r = sr.Recognizer
-
+engine = pyttsx3.init()
 while True:
     try:
         with sr.Microphone() as source:
@@ -11,6 +12,9 @@ while True:
             text = text.lower()
 
             print(f"Recognized text:{text}")
+            if 'megaman' in text:
+                text = text.replace('megaman', '')
+                print(text)
 
     except:
         print("No audio detected")
