585b53054aad477a8c631d6ebe22213dab730fe8
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-15 13:27:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
 r = sr.Recognizer
 engine = pyttsx3.init()
+bot_name = ['megaman', 'mega man']
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
 while True:
     try:
         with sr.Microphone() as source:
-            print("Say something")
+            print("Listening...")
             audio = r.listen(source)
             text = r.recognize_google(audio)
             text = text.lower()
 
-            print(f"Recognized text:{text}")
-            if 'megaman' in text:
-                text = text.replace('megaman', '')
-                print(text)
+            if 'megaman' or 'mega man' in text:
+                for x in bot_name:
+                    text = text.replace(x, '')
+            if 'play' in text:
+                song = text.replace('play', '')
+                talk('playing ' + song)
+                pywhatkit.playonyt(song)
 
     except:
-        print("No audio detected")
         r = sr.Recognizer()
         continue
